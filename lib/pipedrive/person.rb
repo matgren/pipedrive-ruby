9a400d3a8647240ad5b0dfc5c735b53276484b95
Mat Gren <maciej.gren@gmail.com>
2014-04-17 12:39:39 +0200

    added filter_all method to get a list of people filtered by custom filter."

--- lib/pipedrive/person.rb
+++ lib/pipedrive/person.rb
@@ -2,7 +2,11 @@ module Pipedrive
   class Person < Base
 
     class << self
-
+      
+      def filter_all(filter_id, opts = {:start => 0, :sort_mode => 'asc'})
+        Person.all( get("/persons", :query => opts.merge(:filter_id => filter_id) ) )
+      end
+      
       def find_or_create_by_name(name, opts={})
         find_by_name(name, :org_id => opts[:org_id]).first || create(opts.merge(:name => name))
       end
@@ -12,5 +16,7 @@ module Pipedrive
     def deals()
       Deal.all(get "#{resource_path}/#{id}/deals", :everyone => 1)
     end
+    
+  
   end
 end
